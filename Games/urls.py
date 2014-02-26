19c2e79976beb0e0d458d8d5cbd034dab745b183
PSpeiser <mail@patrickspeiser.at>
2014-02-26 16:33:30 +0100

    Reformat Source

--- Games/urls.py
+++ Games/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import *
 import views
+
 urlpatterns = patterns('',
-    (r'^$',views.games))
+                       (r'^$', views.games))
--- Games/views.py
+++ Games/views.py
@@ -2,39 +2,44 @@ from django.views.decorators.cache import cache_page
 from django.shortcuts import render
 
 import os
-TEMPLATE_DIRS = (os.path.join(os.path.dirname(__file__), '..', 'Games', 'templates').replace('\\','/'),)
 
-from datetime import date,datetime
+TEMPLATE_DIRS = (os.path.join(os.path.dirname(__file__), '..', 'Games', 'templates').replace('\\', '/'),)
+
+from datetime import date, datetime
+
 year = date.today().year
-goodPlatforms = ['Win','X360','PS3']
+goodPlatforms = ['Win', 'X360', 'PS3']
 
-@cache_page(60*60*6) #cache for 6 hours
+
+@cache_page(60 * 60 * 6) #cache for 6 hours
 def games(request):
     games = get_games_from_wikipedia()
-    games.append({'date':datetime.now().strftime('%Y-%m-%d'),
-        'name':u'Today',
-        'platforms':'Date'})
-    games.sort(key=lambda x:x['date'])
-    return render(request, 'games.html',{'games':games,
-                'filter':goodPlatforms,
-                'year':year})
+    games.append({'date': datetime.now().strftime('%Y-%m-%d'),
+                  'name': u'Today',
+                  'platforms': 'Date'})
+    games.sort(key=lambda x: x['date'])
+    return render(request, 'games.html', {'games': games,
+                                          'filter': goodPlatforms,
+                                          'year': year})
 
 
 #This gets the game releases from http://en.wikipedia.org/wiki/2014_in_video_gaming?printable=yes
 #Ugly Code
 def get_games_from_wikipedia():
     import urllib2
+
     opener = urllib2.build_opener()
     opener.addheaders = [('User-agent', 'Mozilla/5.0')]
     url = "http://en.wikipedia.org/wiki/%s_in_video_gaming?printable=yes" % year
     page = opener.open(url).read()
     from bs4 import BeautifulSoup
+
     soup = BeautifulSoup(page)
     tables = soup.find_all('table')
     #find tables
     tables = [table for table in tables
               if table.get('class')
-              and 'wikitable' in table.get('class')]
+        and 'wikitable' in table.get('class')]
     #only use the tables with release dates and platforms in them
     tables = [table for table in tables
               if 'Platform(s)' in (table.findChild('tr').text)]
@@ -57,25 +62,25 @@ def get_games_from_wikipedia():
                 continue
             name = elements[-2].text
             platforms = elements[-1].text
-            game = {'date':gamedate,
-                    'name':name,
-                    'platforms':platforms}
+            game = {'date': gamedate,
+                    'name': name,
+                    'platforms': platforms}
             games.append(game)
             lastDate = gamedate
 
     #add proper dates
     import calendar
+
     month = 1
     last_date = 0
     for game in games:
         if game['date'] < last_date:
             month += 1
         if game['date'] == 32:
-            game['date'] = calendar.monthrange(year,month)[1]
+            game['date'] = calendar.monthrange(year, month)[1]
         last_date = game['date']
-        game['date'] = '-'.join([str(year),str(month).rjust(2,'0'),str(game['date']).rjust(2,'0')])
+        game['date'] = '-'.join([str(year), str(month).rjust(2, '0'), str(game['date']).rjust(2, '0')])
 
     games = [game for game in games if any(goodPlatform in game['platforms'] for goodPlatform in goodPlatforms)]
-
 
     return games
