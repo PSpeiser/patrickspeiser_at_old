5b4b786f8d9f8805dca2919e65f2bdc64f393651
PSpeiser <mail@patrickspeiser.at>
2014-03-03 12:49:06 +0100

    Added filtering by rating,shelved and number of ratings

--- BookSearcher/booksearcher.py
+++ BookSearcher/booksearcher.py
@@ -1,12 +1,13 @@
 import xmltodict
 import urllib2
 from json_functions import json_response
-from models import Book,Shelf,Shelved,QueuedBook
+from models import Book, Shelf, Shelved, QueuedBook
 from django.http import HttpResponse
 from django.shortcuts import render
 
 
 key = 'KMi0fKJU9NYgHcb9vMGA7A'
+
 
 def books(request):
     suggestions = ["fantasy",
@@ -18,15 +19,16 @@ def books(request):
                    "programming",
                    "computer-science",
                    "non-fiction"]
-    return render(request,'books.html',{'suggestions':suggestions})
+    return render(request, 'books.html', {'suggestions': suggestions})
+
 
 def book_json(request, bookid):
     book = get_book(bookid)
-    return json_response(request,book.dict)
+    return json_response(request, book.dict)
 
 
 def get_book(bookid):
-    book,created = Book.objects.get_or_create(goodreads_id=bookid)
+    book, created = Book.objects.get_or_create(goodreads_id=bookid)
     if created:
         url = 'https://www.goodreads.com/book/show/%s?format=xml&key=%s' % (bookid, key)
         s = urllib2.urlopen(url).read()
@@ -46,10 +48,10 @@ def get_book(bookid):
             if xml['popular_shelves']:
                 for xml_shelf in xml['popular_shelves']['shelf']:
                     try:
-                        shelf,created = Shelf.objects.get_or_create(name= xml_shelf['@name'])
+                        shelf, created = Shelf.objects.get_or_create(name=xml_shelf['@name'])
                         if created:
                             shelf.save()
-                        shelved,created = Shelved.objects.get_or_create(book = book,shelf=shelf)
+                        shelved, created = Shelved.objects.get_or_create(book=book, shelf=shelf)
                         shelved.shelved_times = xml_shelf['@count']
                         shelved.save()
                     except:
@@ -57,40 +59,46 @@ def get_book(bookid):
     return book
 
 
-def shelf(request,shelf_name):
+def shelf(request, shelf_name):
     try:
+        min_rating = float(request.GET.get('min_rating', 0))
+        min_shelved = int(request.GET.get('min_shelved', 1))
+        min_ratings = int(request.GET.get('min_ratings', 0))
         shelf = Shelf.objects.get(name=shelf_name)
         books = []
-        for shelved in shelf.shelved_set.all():
-            books.append({'title':shelved.book.title,
-                          'author':shelved.book.author,
-                          'average_rating':shelved.book.average_rating,
-                          'ratings_count':shelved.book.ratings_count,
-                          'score':shelved.book.ratings_sum,
-                          'shelved': shelved.shelved_times,
-                          'url':'https://www.goodreads.com/book/show/%s' % shelved.book.goodreads_id})
+        for shelved in shelf.shelved_set.filter(shelved_times__gte=min_shelved,
+                                                book__ratings_count__gte=min_ratings):
+            if shelved.book.average_rating >= min_rating:
+                books.append({'title': shelved.book.title,
+                              'author': shelved.book.author,
+                              'average_rating': shelved.book.average_rating,
+                              'ratings_count': shelved.book.ratings_count,
+                              'score': shelved.book.ratings_sum,
+                              'shelved': shelved.shelved_times,
+                              'url': 'https://www.goodreads.com/book/show/%s' % shelved.book.goodreads_id})
     except:
         books = []
 
+    return render(request, 'shelf.html', {'books': books, 'shelf': shelf_name})
 
-    return render(request,'shelf.html',{'books':books,'shelf':shelf_name})
 
-def shelf_json(request,shelf_name):
+def shelf_json(request, shelf_name):
     shelf = Shelf.objects.get(name=shelf_name)
-    return json_response(request,shelf.dict)
+    return json_response(request, shelf.dict)
 
 
 maxbooks = 1000
 maxpages = maxbooks / 20
 
-def search_shelf(request,shelf_name):
+
+def search_shelf(request, shelf_name):
     response = HttpResponse(search_shelf_internal(shelf_name))
     response['X-Accel-Buffering'] = "no"
     return response
 
 
 def search_shelf_internal(shelf_name):
-    url = 'https://www.goodreads.com/search.xml?key=%s&q=%s&page=' % (key,shelf_name)
+    url = 'https://www.goodreads.com/search.xml?key=%s&q=%s&page=' % (key, shelf_name)
     yield "<div>Searching for %s</div>" % shelf_name
     page = 1
     totalresults = 1
@@ -101,11 +109,11 @@ def search_shelf_internal(shelf_name):
         xml = xmltodict.parse(s)
         totalresults = int(xml['GoodreadsResponse']['search']['total-results'])
         receivedresults = int(xml['GoodreadsResponse']['search']['results-end'])
-        yield "<div>Retrieved %s of %s results</div>" % (receivedresults,totalresults)
+        yield "<div>Retrieved %s of %s results</div>" % (receivedresults, totalresults)
         works = xml['GoodreadsResponse']['search']['results']['work']
         for work in works:
             try:
-                queuedBook = QueuedBook(goodreadsid = work['best_book']['id']['#text'])
+                queuedBook = QueuedBook(goodreadsid=work['best_book']['id']['#text'])
                 queuedBook.save()
             except:
                 pass
